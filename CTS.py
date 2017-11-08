ecec7bf56d7650bc7180343a7b674a4dc48935cc
chenchen <co1in@outlook.com>
2017-11-08 22:17:51 +0800

    fix incorrect package path

--- CTS.py
+++ CTS.py
@@ -147,23 +147,24 @@ class PackageManager:
                 # upload to oss
                 sleep(2)
                 print('pushing {0}'.format(pkg.file_name))
-                total_size = os.path.getsize(pkg.file_name)
+                local_file_path = os.path.join(self.base_path, 'packages', pkg.file_name)
+                total_size = os.path.getsize(local_file_path)
                 part_size = determine_part_size(total_size, preferred_size=100 * 1024)
-                upload_id = bucket.init_multipart_upload(pkg.file_name).upload_id
+                upload_id = bucket.init_multipart_upload(local_file_path).upload_id
                 parts = []
-                with open(pkg.file_name, 'rb') as fileobj:
+                with open(local_file_path, 'rb') as fileobj:
                     part_number = 1
                     offset = 0
                     while offset < total_size:
                         num_to_upload = min(part_size, total_size - offset)
-                        result = bucket.upload_part(pkg.file_name, upload_id, part_number,
+                        result = bucket.upload_part(local_file_path, upload_id, part_number,
                                                     SizedFileAdapter(fileobj, num_to_upload))
                         parts.append(PartInfo(part_number, result.etag))
                         offset += num_to_upload
                         part_number += 1
-                bucket.complete_multipart_upload(pkg.file_name, upload_id, parts)
+                bucket.complete_multipart_upload(local_file_path, upload_id, parts)
                 with open(pkg.file_name, 'rb') as fileobj:
-                    assert bucket.get_object(pkg.file_name).read() == fileobj.read()
+                    assert bucket.get_object(local_file_path).read() == fileobj.read()
                 bucket.put_object_acl(pkg.file_name, oss2.OBJECT_ACL_PUBLIC_READ)
                 os.remove(os.path.join(self.base_path, 'packages', pkg.file_name))
 
